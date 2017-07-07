e5f8dcfe41e0d2dd6a7c90bf8b54e346c0caae7d
Nick Alan DAmico <nickalan82@icloud.com>
2017-07-07 04:12:17 +0000

    Adding to CLI interface

--- lib/local_ski_report/cli.rb
+++ lib/local_ski_report/cli.rb
@@ -11,14 +11,29 @@ class LocalSkiReport::CLI
     ]
     
     def call
-        puts "Find your Local Ski Report:"
-        list_regions
-        puts "Which region would you like to check? type number: "
-        x = gets.chomp.to_i
-        list_states(x)
-        puts "Which State would you like to check? type number: "
-        selected_state = gets.chomp.to_i
-        # This user Response will be used to make a request 
+        input = nil
+        while input != 'exit'    
+            puts "Find your Local Ski Report:"
+            list_regions
+            puts "Which region would you like to check? type number: "
+            x = gets.chomp.to_i
+            list_states(x)
+            puts "Which State would you like to check? type number: "
+            selected_state = gets.chomp.to_i
+            # This user Response will be used to make a request
+            puts "1. Ober Gatlinburg"
+            puts "2. North Ridge Resort"
+            puts "Which Ski Resort would you like info on? "
+            puts "Ober Gatlinburg - Closed - 0/4 Slopes Open - 0\" of New Snow Fall"
+            puts "Type: 'more' to see detailed report, 'new' to select New Resort, 'exit' to leave App."
+            input = gets.strip.downcase
+            if input == "new"
+                self.call
+            elsif input == "more"
+                puts "More info Report..."
+            end
+        end
+        exit_msg
     end
     
     def list_regions
@@ -35,6 +50,11 @@ class LocalSkiReport::CLI
         STATES_WITH_RESORTS[user_request].each_value do |states|
             states.each_with_index { |state, i| puts "#{i+1}. #{state}" }
         end
+    end
+    
+    def exit_msg
+        puts "Check back later for latest Ski reports, "
+        puts "Thanks for using Local Ski Reports gem."
     end
     
 end
