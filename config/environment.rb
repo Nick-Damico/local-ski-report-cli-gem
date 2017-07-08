7245dfb26bd91c3d020af1e8a1ca560d4d503d84
Nick Alan DAmico <nickalan82@icloud.com>
2017-07-08 15:32:37 +0000

    Added resorts.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # require "pry"
+# require "terminal-table"
 # require "nokogiri"
 # require "open-uri"
 
 require_relative '../lib/local_ski_report'
+require_relative '../lib/local_ski_report/resorts'
 require_relative '../lib/local_ski_report/cli'
