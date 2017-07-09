7c10309ca90844e905a2a9747e7554a77f18d4fa
Nick Alan DAmico <nickalan82@icloud.com>
2017-07-09 12:32:24 +0000

    Added require for ruby gems

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'terminal-table'
+
 require_relative '../lib/local_ski_report'
 require_relative '../lib/local_ski_report/resort'
 require_relative '../lib/local_ski_report/cli'
