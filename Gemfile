a4557bc14a62904d4718eb12714cfffb41bccf7e
Nick Alan DAmico <nickalan82@icloud.com>
2017-07-09 12:16:22 +0000

    Moved required gems to .gemspec file

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in local_ski_report.gemspec
 gemspec
+
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,3 @@
-# require "pry"
-# require "terminal-table"
-# require "nokogiri"
-# require "open-uri"
-
 require_relative '../lib/local_ski_report'
 require_relative '../lib/local_ski_report/resort'
 require_relative '../lib/local_ski_report/cli'
