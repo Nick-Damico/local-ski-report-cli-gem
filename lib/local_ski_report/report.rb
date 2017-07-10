6c6a88d14558dfda2d6e3f0f8c8cdd3a96b70b4e
Nick Alan DAmico <nickalan82@icloud.com>
2017-07-10 22:29:39 +0000

    Cleaning up Classes, adding some possibly useful future instance Methods for Report Class

--- lib/local_ski_report/report.rb
+++ lib/local_ski_report/report.rb
@@ -1,8 +1,24 @@
 class LocalSkiReport::Report
     attr_accessor :date, :resort, :url, :lifts_open, :new_snow, :base
     
-    def initialize(date)
-        @date = date
+    @@all = []
+    
+    def initialize
+        @@all << self
+    end
+    
+    def get_brief_report
+    end
+    
+    def get_full_report
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def self.clear
+        @@all.clear
     end
     
 end
