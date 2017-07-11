6bf192b857af96f3b5e6b409de891afe9d2cbaea
Nick Alan DAmico <nickalan82@icloud.com>
2017-07-11 00:18:43 +0000

    .scrap_resorts_page loops html content instantiating Report and Resort Class instances

--- lib/local_ski_report/scraper.rb
+++ lib/local_ski_report/scraper.rb
@@ -7,9 +7,43 @@ class LocalSkiReport::Scraper
    
     def self.scrap_resorts_page(state_url)
         url = "http://www.onthesnow.com/#{state_url}/skireport.html"
-        doc = self.get_resorts_page(url)
+        doc = self.get_page(url)
+        table = doc.css('table')
+        rows = table.css('tr')
         
-        binding.pry
+        rows.collect do |resort|
+            # Build Resort Object
+            new_resort = LocalSkiReport::Resort.new
+            new_resort.name = resort.css('td div.name').text
+            # Need to Check which url gives full report
+            new_resort.url = resort.css('div.name a').first['href']
+            new_resort.location = resort.css('td div.rRegion').text
+            new_resort.lifts =  resort.css('td')[4].text.gsub("/","")
+            
+            # Build Report Object
+            new_report = LocalSkiReport::Report.new
+            new_report.date = resort.css('div.lUpdate').text
+            new_report.url = resort.css('td')[3].css('a').first['href']
+            status = resort.css('td')[1].css('div').first['class'].split(" ").pop
+            if status == "stateD1"
+                new_report.status = "Open"
+            elsif status == "stateD2"
+                new_report.status = "Closed"
+            else
+                new_report.status = "Weekends"
+            end
+            new_report.new_snow = resort.css('td')[2].css('b')[0].text
+            new_report.base = resort.css('td')[3].css('b')[0].text
+            new_report.lifts_open = resort.css('td')[4].text
+            
+            # Collaborate Resort Instance and Report
+            # Resort 'has many' Reports
+            # A Report 'belongs to a' Resort
+            new_resort.reports << new_report
+            new_report.resort = new_resort
+            new_resort
+        end
+        
     end
     
     def self.scrap_report_page(report_url)
