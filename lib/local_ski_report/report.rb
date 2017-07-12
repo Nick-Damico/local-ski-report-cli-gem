b25267f2c356b50d8adeb7d1294cf02740597060
Nick Alan DAmico <nickalan82@icloud.com>
2017-07-12 16:25:05 +0000

    For displaying table of extended info, defined #get_xt_report, #get_ticket_prices, #xt_report, updated #report

--- lib/local_ski_report/report.rb
+++ lib/local_ski_report/report.rb
@@ -1,5 +1,5 @@
 class LocalSkiReport::Report
-    attr_accessor :date, :url, :status, :new_snow, :base, :lifts_open, :resort
+    attr_accessor :date, :url, :status, :new_snow, :base, :lifts_open, :resort, :elevation, :trails, :tickets
     
     @@all = []
     
@@ -39,13 +39,36 @@ class LocalSkiReport::Report
         end
     end
     
-    def report(resort)
-        rows = []
-        rows << [resort.name,{:value => self.date, :alignment => :center}, self.status,{:value => self.new_snow, :alignment => :center},{:value => self.base, :alignment => :center}, {:value => "#{self.lifts_open}/#{resort.lifts}", :alignment => :right}]
-        Terminal::Table.new :title => "Ski Report", :headings => ['Resort Name', 'Updated On' 'Status', 'New Snow', 'Base Depth', 'Lifts Open'], :rows => rows
+    def get_xt_report_info(html)
+        table = html.css('table')
+        rows = table.css('tr')
+        self.elevation = rows[1].css('td').text.split(" - ")
+        self.trails = rows[2].css('td').text.gsub("|","").split
+        self.tickets = get_ticket_prices(rows[4])
+        
     end
     
-    def xt_report(resort)
+    def get_ticket_prices(row)
+        arr = row.css('td').text.gsub("US","").split
+        price_arr = [arr[1], arr[3]]
+        price_arr
+    end
+    
+    def report(resort)
+        rows = []
+        rows << [resort.name, {:value => self.date, :alignment => :center}, self.status, {:value => self.new_snow, :alignment => :center},{:value => self.base, :alignment => :center}, {:value => "#{self.lifts_open}/#{resort.lifts}", :alignment => :right}]
+        Terminal::Table.new :title => "Ski Report", :headings => ['Resort Name', 'Updated On', 'Status', 'New Snow', 'Base Depth', 'Lifts Open'], :rows => rows
+    end
+    
+
+    def xt_report
+        rows = []
+        rows << ['ELEVATION', "Base: #{elevation[0]}", "-" , "Summit: #{elevation[1]}"]
+        rows << ["TRAILS", "Beginner: #{trails[0]}", "Intermediate: #{trails[1]}", "Advanced: #{trails[2]}", "Expert: #{trails[3]}"]
+        rows << ["TICKET PRICES", "From: #{tickets[0]}", "to #{tickets[1]}"]
+        table = Terminal::Table.new :title => "EXTEND SKI REPORT INFO.", :rows => rows
+        table.style = {:all_separators => true}
+        table
     end
     
 end
