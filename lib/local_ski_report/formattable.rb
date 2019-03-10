4e41ab1d5a1fe3b6a8a32c4cd55bfd0808c14da4
Nick-Damico <nickalan82@icloud.com>
2019-03-10 12:52:51 -0400

    Add #numbered_collection, that takes a collection and returns a numerical list of the collection. test passing added to _spec.rb file

--- lib/local_ski_report/formattable.rb
+++ lib/local_ski_report/formattable.rb
@@ -2,4 +2,8 @@ module Formattable
   def separator(num)
     puts '-' * num
   end
+
+  def numbered_collection(collection)
+    collection.collect.with_index(1) { |item, i| "#{i}. #{item}"}
+  end
 end
--- spec/local_ski_report/formattable_spec.rb
+++ spec/local_ski_report/formattable_spec.rb
@@ -6,9 +6,17 @@ end
 
 RSpec.describe Formattable do
   describe '#separator' do
-    it 'it outputs a dashed line separator' do
+    it 'outputs a dashed line separator' do
       dc = DummyClass.new
       expect { dc.separator(5) }.to output("-----\n").to_stdout
+    end
+  end
+
+  describe '#numbered_collection' do
+    it 'formats a collection into a numbered collection' do
+      dc = DummyClass.new
+      region_collection = LocalSkiReport::Regions.all_states_in_region(2)
+      expect(dc.numbered_collection(region_collection)).to eq(['1. Alaska', '2. Idaho', '3. Oregon', '4. Washington'])
     end
   end
 end
