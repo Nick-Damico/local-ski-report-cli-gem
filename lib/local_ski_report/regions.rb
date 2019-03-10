184d85c74452dbf177c3934af992bcd183f86e21
Nick-Damico <nickalan82@icloud.com>
2019-03-10 16:53:00 -0400

    Restructure constant REGIONS_WITH_RESORTS to a hash. Refactored .all_regions, .all_states_in_regions. Updated tests.

--- lib/local_ski_report/regions.rb
+++ lib/local_ski_report/regions.rb
@@ -1,24 +1,20 @@
 class LocalSkiReport::Regions
-  REGIONS_WITH_RESORTS = [
-    { midwest: %w[Illinois Indiana Iowa Kansas Michigan Minnesota Missouri Ohio Wisconsin] },
-    { northeast: %w[Connecticut Maine Massachusetts New Hampshire New Jersey New York Pennsylvania Rhode Island Vermont] },
-    { northwest: %w[Alaska Idaho Oregon Washington] },
-    { rockies: %w[Colorado Montana New Mexico Utah Wyoming] },
-    { southeast: %w[Alabama Georgia Maryland North Carolina Tennessee Virginia West Virginia] },
-    { west_coast: %w[Arizona California Nevada] }
-  ].freeze
+  REGIONS_WITH_RESORTS = {
+    midwest: %w[Illinois Indiana Iowa Kansas Michigan Minnesota Missouri Ohio Wisconsin],
+    northeast: %w[Connecticut Maine Massachusetts New Hampshire New Jersey New York Pennsylvania Rhode Island Vermont],
+    northwest: %w[Alaska Idaho Oregon Washington],
+    rockies: %w[Colorado Montana New Mexico Utah Wyoming],
+    southeast: %w[Alabama Georgia Maryland North Carolina Tennessee Virginia West Virginia],
+    west_coast: %w[Arizona California Nevada]
+  }.freeze
 
   def self.all_regions
-    REGIONS_WITH_RESORTS.map do |region|
-      region.each_key.map do |k|
-        k.to_s.tr('_', '').upcase
-      end
-    end.flatten
+    REGIONS_WITH_RESORTS.map { |region, v| region.to_s.tr('_', '').upcase }
   end
 
-  def self.all_states_in_region(num)
-    REGIONS_WITH_RESORTS[num].each_value.map do |states|
-      states.map { |state| state }      
-    end.flatten
+  def self.all_states_in_region(region_number)
+    # convert string variable to symbol to access value in REGIONS_WITH_RESORTS
+    region_name = all_regions[region_number].downcase.to_sym
+    REGIONS_WITH_RESORTS[region_name]
   end
 end
--- spec/local_ski_report/output_spec.rb
+++ spec/local_ski_report/output_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe LocalSkiReport::Output do
   end
   describe '.list_regions' do
     it 'outputs to terminal a numbered list of regions' do
-      expect { @output.list_regions }.to output("1. MIDWEST\n2. NORTHEAST\n3. NORTHWEST\n4. ROCKIES\n5. SOUTHEAST\n6. WESTCOAST\n").to_stdout      
+      expect { @output.list_regions }.to output("1. MIDWEST\n2. NORTHEAST\n3. NORTHWEST\n4. ROCKIES\n5. SOUTHEAST\n6. WESTCOAST\n").to_stdout
+    end
+  end
+
+  describe '.list_states' do
+    it 'outputs to terminal a numbered list of states from a region' do
+      
     end
   end
 end
