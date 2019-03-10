249dd0ec3c4d4023996e23a59fc8a4195e846017
Nick-Damico <nickalan82@icloud.com>
2019-03-10 12:48:42 -0400

    updated to 0.15

--- spec/local_ski_report/output_spec.rb
+++ spec/local_ski_report/output_spec.rb
@@ -1,9 +1,12 @@
 require "spec_helper"
 
-RSpec.describe Regions do
-  describe 'LocalSkiResort::Output' do
-    it '' do
-
+RSpec.describe LocalSkiReport::Output do
+  before(:each) do
+    @output = LocalSkiReport::Output.new
+  end
+  describe '.list_regions' do
+    it 'outputs to terminal a numbered list of regions' do
+      expect { @output.list_regions }.to output("1. MIDWEST\n2. NORTHEAST\n3. NORTHWEST\n4. ROCKIES\n5. SOUTHEAST\n6. WESTCOAST\n").to_stdout      
     end
   end
 end
