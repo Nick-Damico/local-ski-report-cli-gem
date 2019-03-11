068a3add97d33d095040f5a5536a22f231ecc8b9
Nick-Damico <nickalan82@icloud.com>
2019-03-11 10:47:39 -0400

    update test msg

--- spec/local_ski_report/regions_spec.rb
+++ spec/local_ski_report/regions_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe LocalSkiReport::Regions do
     it 'has a constant REGIONS_WITH_RESORTS' do end
 
     describe '.all_regions' do
-      it 'returns an array of numbered, string formatted region names' do
+      it 'returns an array region names' do
         expect(LocalSkiReport::Regions.all_regions).to eq(%w[MIDWEST NORTHEAST NORTHWEST ROCKIES SOUTHEAST WESTCOAST])
       end
     end
 
     describe '.all_states_in_region' do
-      it 'returns an array of numbered, string formatted state names' do
+      it 'returns an array of state names from a region' do
         # The parameter '2' refers to the region Northwest
         expect(LocalSkiReport::Regions.all_states_in_region(2)).to eq(%w[Alaska Idaho Oregon Washington])
       end
