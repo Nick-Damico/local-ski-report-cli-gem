18dcaffc7e4e133af7adcece10ce8025ed4ae117
Nick-Damico <nickalan82@icloud.com>
2019-03-13 13:04:28 -0400

    Add #format_to_string, #downcase_to_sym

--- lib/local_ski_report/formattable.rb
+++ lib/local_ski_report/formattable.rb
@@ -6,4 +6,12 @@ module Formattable
   def numbered_collection(collection)
     collection.collect.with_index(1) { |item, i| "#{i}. #{item}"}
   end
+
+  def format_to_string(sym)
+    sym.to_s.tr('_', '').upcase
+  end
+
+  def downcase_to_sym(string)
+    string.downcase.to_sym
+  end
 end
