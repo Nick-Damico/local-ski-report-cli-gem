8dc49b0794354d180348e435ea4547f3461a305a
Nick-Damico <nickalan82@icloud.com>
2019-03-22 11:46:07 -0400

    Removed old #select_resort, #list_resorts, #get_state, #get_key. Define #init, #select_region_state, #resort_selection for prompting a User to select a resort from their choosen Region and State.

--- lib/local_ski_report/cli.rb
+++ lib/local_ski_report/cli.rb
@@ -15,45 +15,30 @@ class LocalSkiReport::CLI
         scraper: @scraper)
     end
 
+    def init
+      scraper.scrape_usa_resorts
+      self.call
+    end
+
     def call
       output.greeting
+      state_name = select_region_state
+      resort_selection(state_name)
+    end
+
+    def select_region_state
       region_num = output.select_region
       state_num = output.select_state(region_num)
-      state_name = region.get_state(region_num, state_num)      
-      # scraper.scrape_usa_resorts
+      # Returns state name ex. 'Tennessee'
+      region.get_state(region_num, state_num)
+    end
+
+    def resort_selection(state_name)
+      resorts = LocalSkiReport::Resort.find_by_location(state_name)
+      output.select_resort(resorts)
     end
 
     def menu
       output.list_regions
-    end
-
-    def get_key(num)
-      STATES_WITH_RESORTS[num].keys[0]
-    end
-
-    def get_state(reg_num, user_reg, st_num)
-      STATES_WITH_RESORTS[reg_num][user_reg][st_num] + ', USA'
-    end
-
-    def list_resorts(state)
-      resorts = LocalSkiReport::Resort.find_by_location(state)
-      # resorts = LocalSkiReport::Scraper.scrap_resorts_page(state)
-      resorts.each.with_index(1) { |r, i| puts "#{i}. #{r.name}" }
-      resorts
-    end
-
-    def select_resort(state)
-      resorts_arr = list_resorts(state)
-      separator(50)
-      puts 'Select a Resort or Ski-Area for the latest Report: '
-      input = gets.chomp.to_i - 1
-      if input.between?(0, resorts_arr.size - 1)
-        @resort = resorts_arr[input]
-      else
-        separator(60)
-        puts "Invalid Choice. Please choose a Resort # between 1 - #{resorts_arr.size}."
-        separator(60)
-        select_resort(state)
-      end
     end
 end
