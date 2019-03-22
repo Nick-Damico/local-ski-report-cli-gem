1cbc27e39e081f7df9222f73891e548e9b4d5898
Nick-Damico <nickalan82@icloud.com>
2019-03-22 14:59:07 -0400

    Removed old .create, #add_report methods

--- lib/local_ski_report/resort.rb
+++ lib/local_ski_report/resort.rb
@@ -15,25 +15,12 @@ class  LocalSkiReport::Resort
         @@all << self
     end
 
-    def add_report(report)
-        report.resort = self
-        self.reports << report
-    end
-
     def self.all
         @@all
     end
 
     def self.clear
         @@all.clear
-    end
-
-    def self.create(html)
-        name = html.css('td div.name').text
-        url = html.css('div.name a').first['href']
-        location = html.css('td div.rRegion').text
-        lifts =  html.css('td')[4].text.split("/").last
-        self.new(name, url, location, lifts)
     end
 
     def self.find_by_location(location)
