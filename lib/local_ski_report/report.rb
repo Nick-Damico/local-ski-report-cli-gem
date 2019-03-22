133f6d85fb50f27c25ceba4ba7c8ad99456e4502
Nick-Damico <nickalan82@icloud.com>
2019-03-22 15:01:00 -0400

    attr_reader replaces attr_accessor method. #initialize uses || operator to replace values if nil. Update #report to reflect changes of report attributes.

--- lib/local_ski_report/report.rb
+++ lib/local_ski_report/report.rb
@@ -1,48 +1,49 @@
 class LocalSkiReport::Report
-    attr_accessor :base_depth, :lifts, :location, :name, :open_acreage, :snowfall, :url
+  attr_reader :base_depth, :l_update, :lifts, :location, :name, :snowfall_past_24, :snowfall_past_72, :open_acreage, :snowfall, :url
 
-    @@all = []
+  @@all = []
 
-    def initialize(resort)
-      @base_depth = resort.base_depth
-      @lifts = resort.lifts
-      @l_update = resort.l_update
-      @open_acreage = resort.open_acreage
-      @name = resort.name
-      @snowfall = resort.snowfall
-      @url = resort.url
-      @@all << self
-    end
+  def initialize(resort)
+    @base_depth = resort.base_depth
+    @lifts = resort.lifts
+    @l_update = resort.l_update || 'N/A'
+    @open_acreage = resort.open_acreage
+    @name = resort.name
+    @snowfall_past_24 = resort.snowfall[:twenty_four] || '0'
+    @snowfall_past_72 = resort.snowfall[:seventy_two] || '0'
+    @url = resort.url
+    @@all << self
+  end
 
-    def self.all
-        @@all
-    end
+  def self.all
+    @@all
+  end
 
-    def self.clear
-        @@all.clear
-    end
+  def self.clear
+    @@all.clear
+  end
 
-    def report
-        rows = []
-        rows << [self.resort.name, {:value => self.date, :alignment => :center}, self.status, {:value => self.new_snow, :alignment => :center},{:value => self.base, :alignment => :center}, {:value => "#{self.lifts_open}/#{resort.lifts}", :alignment => :right}]
-        Terminal::Table.new :title => "Ski Report", :headings => ['Resort Name', 'Updated On', 'Status', 'New Snow', 'Base Depth', 'Lifts Open'], :rows => rows
-    end
+  def report
+    rows = []
+    rows << [name, { value: l_update, alignment: :center }, { value: "#{@snowfall_past_24}\" - #{@snowfall_past_72}\"", alignment: :center }, { value: base_depth, alignment: :center }, { value: lifts, alignment: :right }]
+    Terminal::Table.new title: 'Ski Report', headings: ['Resort Name', 'Updated On', 'New Snow', 'Base Depth', 'Lifts Open'], rows: rows
+  end
 
-    def get_xt_report_info(html)
-        table = html.css('table')
-        rows = table.css('tr')
-        @elevation = rows[1].css('td').text.split(" - ")
-        @trails = rows[2].css('td').text.gsub("|","").split
-        @tickets = get_ticket_prices(rows[4])
-    end
+  def get_xt_report_info(html)
+    table = html.css('table')
+    rows = table.css('tr')
+    @elevation = rows[1].css('td').text.split(' - ')
+    @trails = rows[2].css('td').text.delete('|').split
+    @tickets = get_ticket_prices(rows[4])
+  end
 
-    def xt_report
-        rows = []
-        rows << ['ELEVATION', "Base:", "#{elevation[0]}", "Summit:", "#{elevation[1]}"]
-        rows << ["TRAILS", "Beginner: #{trails[0]}", "Intermediate: #{trails[1]}", "Advanced: #{trails[2]}", "Expert: #{trails[3]}"]
-        rows << ["TICKET PRICES", "Starting at:", "#{tickets[0]}", {:value => "to", :alignment => :center}, "#{tickets[1]}"]
-        table = Terminal::Table.new :title => "#{self.resort.name} Full Report", :rows => rows
-        table.style = {:all_separators => true}
-        table
-    end
+  def xt_report
+    rows = []
+    rows << ['ELEVATION', 'Base:', elevation[0].to_s, 'Summit:', elevation[1].to_s]
+    rows << ['TRAILS', "Beginner: #{trails[0]}", "Intermediate: #{trails[1]}", "Advanced: #{trails[2]}", "Expert: #{trails[3]}"]
+    rows << ['TICKET PRICES', 'Starting at:', tickets[0].to_s, { value: 'to', alignment: :center }, tickets[1].to_s]
+    table = Terminal::Table.new title: "#{resort.name} Full Report", rows: rows
+    table.style = { all_separators: true }
+    table
+  end
 end
